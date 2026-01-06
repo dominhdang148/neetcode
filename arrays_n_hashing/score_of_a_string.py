5744b8b144bbe0ce2a0dcadcea8225555f873e06
dominhdang148 <myclone235@gmail.com>
2026-01-07 01:16:41 +0700

    Added Score of a String

---
+++ arrays_n_hashing/score_of_a_string.py
@@ -0,0 +1,16 @@
+def scoreOfString(s: str) -> int:
+    """
+        Score of a String
+
+        You are given a string `s`. The score of a string
+        is defined as the sum of the absolute difference between ASCII
+        value of adjacent character.
+
+        Return the score of `s`.
+        """
+
+    sum = 0
+    for i in range(len(s) - 1):
+        sum = sum + abs(ord(s[i]) - ord(s[i+1]))
+
+    return sum
---
+++ main.py
@@ -0,0 +1,3 @@
+from arrays_n_hashing.score_of_a_string import scoreOfString
+
+print(scoreOfString("neetcode"))
